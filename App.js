b4e7bea11df7a2688b18e4533aa2139e8192c86f
Ahmet Subasi <ahmttsubasii@gmail.com>
2023-08-28 15:37:51 +0300

    first foods project

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FoodList from './pages/FoodList';
+import FoodDetail from './pages/FoodDetail';
+import FoodCategoryList from './pages/FoodCategoryList';
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="FoodCategoryList"
+          component={FoodCategoryList}
+          options={{
+            title: 'Categories',
+            headerTitleStyle: {
+              color: '#FFA500',
+            },
+            headerStyle: {
+              backgroundColor:"white",
+            },
+          }}
+        />
+        <Stack.Screen name="FoodList" component={FoodList} options={{
+            title: 'Meals',
+            headerStyle: {
+              backgroundColor:"white",
+            },
+            headerTitleStyle: {
+              color: '#FFA500',
+            },
+            headerTintColor:"#FFA500",
+          }}/>
+        <Stack.Screen name="FoodDetail" component={FoodDetail} options={{
+            title: 'Detail',
+            headerTitleStyle: {
+              color: '#FFA500',
+            },
+            headerStyle: {
+              backgroundColor:"white",
+            },
+            headerTintColor:"#FFA500",
+          }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
